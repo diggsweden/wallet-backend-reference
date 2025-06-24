d50566eede0be8bc9dcf8a99b0171a5b20e7d899
Eric Thelin <extern.eric.thelin@digg.se>
2025-06-24 10:43:15 +0200

    refactor: extract UserService interface
    This interface is free from infrastucture details such as Spring data.
    
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- src/main/java/com/example/demo/infrastructure/spring/data/SpringUserService.java
+++ src/main/java/com/example/demo/infrastructure/spring/data/SpringUserService.java
@@ -12,10 +12,11 @@ import org.slf4j.LoggerFactory;
 
 import com.example.demo.application.exception.NotFoundException;
 import com.example.demo.domain.User;
+import com.example.demo.domain.UserService;
 
 
 // TODO: Vote in which layer we do the mapping
-public class SpringUserService {
+public class SpringUserService implements UserService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SpringUserService.class);
 
@@ -34,6 +35,7 @@ public class SpringUserService {
     LOGGER.info("test");
   }
 
+  @Override
   public User get(UUID id) {
     return repository.findById(id)
         .map(UserMapper::toDomain)
