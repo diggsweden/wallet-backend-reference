c763268b8457d015329aa2c6b12c2f98a2dfd66a
Christoffer Killander <extern.christoffer.killander@digg.se>
2025-08-11 14:59:29 +0200

    EUDIW-627 new mappers for User

--- src/main/java/com/example/demo/application/model/UserDto.java
+++ src/main/java/com/example/demo/application/model/UserDto.java
@@ -5,9 +5,11 @@
 package com.example.demo.application.model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 // TODO validate?
 public record UserDto(
+    UUID id,
     String address,
     String name,
     LocalDate birthDate) {
